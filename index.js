01953ee041fe908e85a93c25065767c28d26c800
Jacob Wejendorp <jacob@wejendorp.dk>
2013-09-19 13:25:20 +0200

    Wrapped middleware in configuration to output direct to html via options

--- index.js
+++ index.js
@@ -9,52 +9,62 @@ var fs     = require('fs')
  * Replace Jade files with compiled Javascript files.
  */
 
-module.exports = function (builder) {
-  // Add the runtime.js to our top-level package's `scripts` array.
-  debug('adding jade-runtime.js to %s', builder.config.name);
+module.exports = function(options) {
+  options = options || {};
 
-  // Add our runtime to the builder, and add a require call for our runtime,
-  // so it's global for all future template functions.
-  var runtime = fs.readFileSync(__dirname + '/runtime.js', 'utf8');
-  builder.addFile('scripts', 'jade-runtime.js', runtime);
-  builder.append('require("' + builder.config.name + '/jade-runtime");\n');
+  function (builder) {
+    // Add the runtime.js to our top-level package's `scripts` array.
+    debug('adding jade-runtime.js to %s', builder.config.name);
 
-  // Before processing any scripts, convert `.jade` files to Javascript.
-  builder.hook('before scripts', compileJade);
-};
+    // Add our runtime to the builder, and add a require call for our runtime,
+    // so it's global for all future template functions.
+    if(!options.toHtml) {
+      var runtime = fs.readFileSync(__dirname + '/runtime.js', 'utf8');
+      builder.addFile('scripts', 'jade-runtime.js', runtime);
+      builder.append('require("' + builder.config.name + '/jade-runtime");\n');
+    }
 
-
-/**
- * Compile jade.
- */
-
-function compileJade (pkg, callback) {
-  // Grab our Jade templates.
-  if (!pkg.config.templates) return callback();
-  var files = pkg.config.templates.filter(filterJade);
-
-  files.forEach(function (file) {
-    debug('compiling: %s', pkg.path(file));
-
-    var fullPath = pkg.path(file);
-
-    // Read and compile our Jade.
-    var string = fs.readFileSync(fullPath, 'utf8')
-      , fn     = jade.compile(string, { client: true, compileDebug: false, filename: fullPath });
-
-    // Add our new compiled version to the package, with the same name.
-    file = file.slice(0, file.length - 5) + '.js';
-    pkg.addFile('scripts', file, 'module.exports = ' + fn);
-  });
-
-  callback();
-}
+    // Before processing any scripts, convert `.jade` files to Javascript.
+    builder.hook('before scripts', compileJade);
+  };
 
 
-/**
- * Filter for .jade files.
- */
+  /**
+   * Compile jade.
+   */
 
-function filterJade (filename) {
-  if (path.extname(filename) === '.jade') return true;
+  function compileJade (pkg, callback) {
+    // Grab our Jade templates.
+    if (!pkg.config.templates) return callback();
+    var files = pkg.config.templates.filter(filterJade);
+
+    files.forEach(function (file) {
+      debug('compiling: %s', pkg.path(file));
+
+      var fullPath = pkg.path(file);
+
+      // Read and compile our Jade.
+      var string = fs.readFileSync(fullPath, 'utf8')
+        , fn     = jade[!options.toHtml ? 'compile', 'render'](string, { client: true, compileDebug: false, filename: fullPath });
+
+      if(typeof fn === 'string') {
+        fn = '\''+escape(fn)+'\'';
+      }
+
+      // Add our new compiled version to the package, with the same name.
+      file = file.slice(0, file.length - 5) + '.js';
+      pkg.addFile('scripts', file, 'module.exports = ' + fn);
+    });
+
+    callback();
+  }
+
+
+  /**
+   * Filter for .jade files.
+   */
+
+  function filterJade (filename) {
+    if (path.extname(filename) === '.jade') return true;
+  }
 }
