258e3ee978490863b847c2174712e58fdf6a80af
Jacob Wejendorp <jacob@wejendorp.dk>
2013-09-19 22:48:06 +0200

    Make options live on instance instead of module-level

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ var fs     = require('fs')
   , path   = require('path')
   , debug  = require('debug')('component-jade');
 
-var options = {};
 /**
  * Replace Jade files with compiled Javascript files.
  */
-module.exports = function(opts) {
-  if(typeof opts.build === 'function') {
-    // is builder instance
-    return inject(opts);
+
+module.exports = function(options) {
+  if(typeof options.build === 'function') {
+    // options is builder instance
+    return inject(options, {});
   } else {
-    options = opts;
-    return inject;
+    return function(builder) {
+      inject(builder, options);
+    };
   }
 };
 
-function inject(builder) {
+function inject(builder, options) {
   if(!options.html) {
     // Add the runtime.js to our top-level package's `scripts` array.
     debug('adding jade-runtime.js to %s', builder.config.name);
@@ -27,46 +28,44 @@ function inject(builder) {
     builder.addFile('scripts', 'jade-runtime.js', runtime);
     builder.append('require("' + builder.config.name + '/jade-runtime");\n');
   }
+  /**
+   * Compile jade.
+   */
+
+  function compileJade (pkg, callback) {
+    // Grab our Jade templates.
+    if (!pkg.config.templates) return callback();
+    var files = pkg.config.templates.filter(filterJade);
+
+    files.forEach(function (file) {
+      debug('compiling: %s', pkg.path(file));
+
+      var fullPath = pkg.path(file);
+
+      // Read and compile our Jade.
+      var string   = fs.readFileSync(fullPath, 'utf8')
+        , compiled = jade[!options.html ? 'compile' : 'render'](string, { client: true, compileDebug: false, filename: fullPath });
+
+      if(options.html) {
+        compiled = escapeHtml(compiled);
+      }
+
+      // Add our new compiled version to the package, with the same name.
+      file = file.slice(0, file.length - 5) + '.js';
+      pkg.addFile('scripts', file, 'module.exports = ' + compiled);
+    });
+
+    callback();
+  }
 
   // Before processing any scripts, convert `.jade` files to Javascript.
   builder.hook('before scripts', compileJade);
 };
 
-/**
- * Compile jade.
- */
-
-function compileJade (pkg, callback) {
-  // Grab our Jade templates.
-  if (!pkg.config.templates) return callback();
-  var files = pkg.config.templates.filter(filterJade);
-
-  files.forEach(function (file) {
-    debug('compiling: %s', pkg.path(file));
-
-    var fullPath = pkg.path(file);
-
-    // Read and compile our Jade.
-    var string   = fs.readFileSync(fullPath, 'utf8')
-      , compiled = jade[!options.html ? 'compile' : 'render'](string, { client: true, compileDebug: false, filename: fullPath });
-
-    if(typeof compiled === 'string') {
-      compiled = escapeHtml(compiled);
-    }
-
-    // Add our new compiled version to the package, with the same name.
-    file = file.slice(0, file.length - 5) + '.js';
-    pkg.addFile('scripts', file, 'module.exports = ' + compiled);
-  });
-
-  callback();
-}
 function escapeHtml( str ) {
-  str = '\''+addslashes(str)+'\'';
+  // escape by adding slashes
+  str = '\''+str.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0')+'\'';
   return str.replace(/\n/g,'\'+\n\''); // line breaks need concatenation
-}
-function addslashes( str ) {
-  return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
 }
 
 /**
