0be4d1e2feeefc4ec5780cadb2ebb982f83d7fce
queckezz <fabian.eichenberger@gmail.com>
2014-01-29 21:56:12 +0100

    add examples

--- example/index.js
+++
@@ -1,4 +0,0 @@
-var template = require('./template')
-  , html     = template({ youAreUsingJade : true });
-
-document.body.innerHTML = html;
---
+++ examples/string/builder.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Builder = require('component-builder');
+var write = require('fs').writeFileSync;
+var jade = require('../..');
+var commonjs = Builder.commonjs;
+var concat = Builder.concat;
+var link = Builder.symlink;
+
+/**
+ * Build.
+ */
+
+var builder = Builder(__dirname);
+
+// Build scripts.
+builder.use(commonjs('scripts'));
+builder.use(concat('scripts'));
+
+// Build templates.
+builder.use(jade('templates', { string: true }));
+builder.use(commonjs('templates'));
+builder.use(concat('templates'));
+
+var file = __dirname
+  + '/build.js';
+
+// Build.
+builder.build(function (err, build) {
+  if (err) throw err;
+
+  var js = build.requirejs;
+  js += build.scripts;
+  js += build.templates;
+  js += build.aliases;
+  write(file, js);
+});
